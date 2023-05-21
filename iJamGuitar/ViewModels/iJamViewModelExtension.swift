10b1afeb3e41b14cc0854d9bb7773c86691c00ef
Ron Jurincie <ronjurincie@gmail.com>
2023-05-21 13:18:29 -0400

    added documentation

--- iJamGuitar/ViewModels/iJamViewModelExtension.swift
+++ iJamGuitar/ViewModels/iJamViewModelExtension.swift
@@ -198,4 +198,38 @@ extension iJamGuitarViewModel
         }
         return nil
     }
+    
+    /// This function calculates and returns the lowest displayed fret above the nut for the activeChord in the activeChordGroup in the activeTuning
+    /// - Returns: Int of the lowest displayed fret above the nut
+    /// Note: must be > the nut Int
+    func getinimumDisplayedFret() -> Int {
+        guard let fretChars = activeChord?.fretMap else { return 0 }
+        var highest = 0
+        var thisFretVal = 0
+        
+        for char in fretChars {
+            switch char {
+                // span does NOT include open string nor muted strings
+            case "x":
+                break
+            case "A":
+                thisFretVal = 11
+            case "B":
+                thisFretVal = 12
+            case "C":
+                thisFretVal = 13
+            case "D":
+                thisFretVal = 14
+            default:
+                if let intValue = char.wholeNumberValue {
+                    thisFretVal = intValue
+                } else {
+                    thisFretVal = 0
+                }
+            }
+            highest = max(highest, thisFretVal)
+        }
+        
+        return highest < 6 ? 1 : max(1, highest - 4)
+    }
 }
--- iJamGuitar/Views/VolumeView.swift
+++ iJamGuitar/Views/VolumeView.swift
@@ -12,6 +12,28 @@ struct VolumeView: View {
     @State private var isEditing = false
     var imageSize = UIDevice.current.userInterfaceIdiom == .pad ? 35.0 : 25.0
     
+    func getSlider() -> some View {
+        Slider(
+            value: $vm.volumeLevel,
+            in: 0...10,
+            onEditingChanged: { editing in
+                isEditing = editing
+                if isEditing == false {
+                    vm.appState?.volumeLevel = NSDecimalNumber(value: vm.volumeLevel)
+                    try? vm.context.save()
+                }
+            })
+    }
+    
+    func getSpeakerImage() -> some View {
+        Image(systemName: vm.isMuted ? "speaker.slash.fill" : "speaker.wave.1")
+            .resizable()
+            .frame(width: imageSize, height: imageSize)
+            .shadow(radius: 10)
+            .foregroundColor(Color.white)
+            .padding(10)
+    }
+    
     var body: some View {
         VStack() {
             Spacer()
@@ -20,42 +42,33 @@ struct VolumeView: View {
                 Spacer()
                 Button(action: {
                     vm.isMuted.toggle()
-                    vm.appState?.isMuted = vm.isMuted
-                   
-                    if (vm.isMuted) {
-                        // save volume level and set to zero
-                        vm.savedVolumeLevel = vm.volumeLevel
-                        vm.volumeLevel = 0.0
-                        vm.appState?.volumeLevel = 0.0
-                    }
-                    else {
-                        // restore volume level if user hasn't changed it from zero
-                        if vm.volumeLevel == 0.0 {
-                            vm.volumeLevel = vm.savedVolumeLevel
-                            vm.appState?.volumeLevel = NSDecimalNumber(value: vm.volumeLevel)                        }
-                    }
+                    updateIsMuted()
                     try? vm.context.save()
                 }) {
-                    Image(systemName: vm.isMuted ? "speaker.slash.fill" : "speaker.wave.1")
-                        .resizable()
-                        .frame(width: imageSize, height: imageSize)
-                        .shadow(radius: 10)
-                        .foregroundColor(Color.white)
-                        .padding(10)
+                    getSpeakerImage()
                 }
-                Slider(
-                    value: $vm.volumeLevel,
-                    in: 0...10,
-                    onEditingChanged: { editing in
-                        isEditing = editing
-                        if isEditing == false {
-                            vm.appState?.volumeLevel = NSDecimalNumber(value: vm.volumeLevel)
-                            try? vm.context.save()
-                        }
-                    })
+                getSlider()
                 Spacer()
             }
             Spacer()
+        }
+    }
+    
+    func updateIsMuted() {
+        vm.appState?.isMuted = vm.isMuted
+       
+        if (vm.isMuted) {
+            // save volume level and set to zero
+            vm.savedVolumeLevel = vm.volumeLevel
+            vm.volumeLevel = 0.0
+            vm.appState?.volumeLevel = 0.0
+        }
+        else {
+            // restore volume level if user hasn't changed it from zero
+            if vm.volumeLevel == 0.0 {
+                vm.volumeLevel = vm.savedVolumeLevel
+                vm.appState?.volumeLevel = NSDecimalNumber(value: vm.volumeLevel)
+            }
         }
     }
 }
